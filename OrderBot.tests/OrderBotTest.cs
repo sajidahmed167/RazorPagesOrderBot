ef7a4065a62e06d2d20f80020d05ab9d65c2ddf8
sajidahmed167 <samee_ahmed247@yahoo.com>
2022-07-15 11:37:09 -0400

    fixed test

--- OrderBot.tests/OrderBotTest.cs
+++ OrderBot.tests/OrderBotTest.cs
@@ -20,39 +20,30 @@ namespace OrderBot.tests
             Assert.True(sInput.Contains("Welcome"));
         }
         [Fact]
-        public void TestShawarama()
-        {
-            Session oSession = new Session("12345");
-            String sInput = oSession.OnMessage("hello")[0];
-            Assert.True(sInput.ToLower().Contains("shawarama"));
-        }
-        [Fact]
-        public void TestSize()
+        public void TestName()
         {
             Session oSession = new Session("12345");
             String sInput = oSession.OnMessage("hello")[1];
-            Assert.True(sInput.ToLower().Contains("size"));
+            Assert.True(sInput.ToLower().Contains("name"));
         }
         [Fact]
-        public void TestLarge()
+
+        public void TestSymptoms()
         {
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            String sInput = oSession.OnMessage("large")[0];
-            Assert.True(sInput.ToLower().Contains("protein"));
-            Assert.True(sInput.ToLower().Contains("large"));
+            String sInput = oSession.OnMessage("sajid")[0];
+            Assert.True(sInput.ToLower().Contains("symptoms"));
         }
         [Fact]
-        public void TestChicken()
+        public void TestDate()
         {
             string sPath = DB.GetConnectionString();
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            oSession.OnMessage("large");
-            String sInput = oSession.OnMessage("chicken")[0];
-            Assert.True(sInput.ToLower().Contains("toppings"));
-            Assert.True(sInput.ToLower().Contains("large"));
-            Assert.True(sInput.ToLower().Contains("chicken"));
+            oSession.OnMessage("sajid");
+            String sInput = oSession.OnMessage("fever")[0];
+            Assert.True(sInput.ToLower().Contains("date"));
         }
     }
 }
